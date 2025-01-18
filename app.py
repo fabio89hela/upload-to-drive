8317bf342c9e6ba5d953e12bc9a4b443eb6d9bc1
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-01-18 11:22:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+import requests
 
 # ID della cartella Google Drive dove salvare i file (sostituisci con il tuo Folder ID)
 FOLDER_ID = "1NjGZpL9XFdTdWcT-BbYit9fvOuTB6W7t"
