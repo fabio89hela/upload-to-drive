ed2c912c6a0c3ecb0f1f468854757e99f4a898dd
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-01-21 10:32:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -125,6 +125,7 @@ if uploaded_file:
 
     # Carica il file convertito su Google Drive
     service = authenticate_drive()
+    st.success(f"Dimensione file: {os.path.getsize(temp_path) / (1024 * 1024)}")
     with st.spinner("Caricamento su Google Drive in corso..."):
         file_id = upload_to_drive(service, output_file_name, temp_path, FOLDER_ID)
         st.success(f"File caricato con successo su Google Drive! IDs del file: {file_id}")
