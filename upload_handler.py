58adac71d7f9f86325230ddd4e6099ab63091dcf
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-01-28 12:16:05 +0100

    Update upload_handler.py

--- upload_handler.py
+++ upload_handler.py
@@ -23,7 +23,6 @@ def upload_to_drive(service, file_name, file_path, folder_id, max_size_mb=20):
     # Lista degli ID dei file caricati
     uploaded_file_ids = []
     if file_size_mb > max_size_mb:
-        st.write("controllo dimensione ok")
         # Crea una directory temporanea per i segmenti
         with tempfile.TemporaryDirectory() as temp_dir:
             segment_prefix = file_name.rsplit('.', 1)[0]  # Nome base senza estensione
@@ -31,25 +30,24 @@ def upload_to_drive(service, file_name, file_path, folder_id, max_size_mb=20):
             # Usa ffmpeg per dividere il file in segmenti più piccoli
             segment_duration =600 #int((max_size_mb * 1024 * 1024) / (file_size_mb / 60))  # Durata stimata in secondi
             try:
-                st.write(file_path)
                 ffmpeg.input(file_path).output(
                     segment_pattern, f="segment", segment_time=segment_duration, c="copy"
                 ).run(overwrite_output=True)
+                st.write("Segmentazione completata.")
             except ffmpeg.Error as e:
                 raise RuntimeError(f"Errore durante la suddivisione del file: {e.stderr.decode()}")
-                st.error(f"{e.stderr.decode()}")
+            segments = os.listdir(temp_dir)
+            if not segments:
+                raise RuntimeError("La segmentazione non ha prodotto alcun file.")
+                st.write(f"Segmenti trovati: {segments}")
             # Carica ogni segmento su Google Drive
-            st.write(os.listdir(temp_dir))
             for segment_file in sorted(os.listdir(temp_dir)):
-                st.write(segment_file)
                 if segment_file.startswith(segment_prefix) and segment_file.endswith(".ogg"):
                     segment_path = os.path.join(temp_dir, segment_file)
                     segment_metadata = {"name": segment_file, "parents": [folder_id]}
                     segment_media = MediaFileUpload(segment_path, resumable=True)
                     file = service.files().create(body=segment_metadata, media_body=segment_media, fields="id").execute()
                     uploaded_file_ids.append(file.get("id"))
-                else:
-                    st.write("siamo qui")
     else:
         # Carica il file intero se non supera il limite
         file_metadata = {"name": file_name, "parents": [folder_id]}
