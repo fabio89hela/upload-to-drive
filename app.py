df2ee9ac8ecb325cad1e3b8a8ba2fd395047017f
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-01-31 17:22:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ if mode == "Carica un file audio":
     with st.form("upload_form"):
         # Caricamento di un file audio locale
         uploaded_file = st.file_uploader("Carica un file audio (MP3, WAV)", type=["mp3", "wav"])
-        submitted = st.form_submit_button("Salva in Drive",disable=True)
+        submitted = st.form_submit_button("Salva in Drive",disabled=True)
         if uploaded_file:
             with st.spinner("Caricando..."):
                 with tempfile.NamedTemporaryFile(delete=False, suffix=f".{uploaded_file.name.split('.')[-1]}") as temp_file:
