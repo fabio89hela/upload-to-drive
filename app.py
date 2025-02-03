72f135c7d8105f603a8a50376550e10ae3af545c
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-02-03 16:10:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ if "transcription" not in st.session_state:
 if "transcription_saved" not in st.session_state:
     st.session_state["transcription_saved"] = False
 
-N8N_WEBHOOK_URL = "https://develophela.app.n8n.cloud/webhook-test/trascrizione" #test link
-#N8N_WEBHOOK_URL = "https://develophela.app.n8n.cloud/webhook/trascrizione" #production link
+#N8N_WEBHOOK_URL = "https://develophela.app.n8n.cloud/webhook-test/trascrizione" #test link
+N8N_WEBHOOK_URL = "https://develophela.app.n8n.cloud/webhook/trascrizione" #production link
 c,FOLDER_ID,regional,nome,domanda1,domanda2=settings_folder("Ematologia")
 
 def convert_mp3_to_wav(input_path, output_path):
