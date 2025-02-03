a01977bedb5bc4ee486e94e5c6a94d8a1824b711
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-02-03 17:58:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -123,6 +123,8 @@ if mode == "Carica un file audio":
     if st.session_state["uploaded_file"]:
         if st.session_state["ricomincia"]==False:
             st.session_state["ricomincia"]=True
+            st.session_state["uploaded_file"]=None
+            st.session_state["avvio"]=True
             st.rerun()
         with st.spinner("Caricando..."):
             with tempfile.NamedTemporaryFile(delete=False, suffix=f".{st.session_state["uploaded_file"].name.split('.')[-1]}") as temp_file:
@@ -180,6 +182,8 @@ if mode == "Carica un file audio":
 elif mode == "Registra un nuovo audio":
     if st.session_state["ricomincia"]==False:
         st.session_state["ricomincia"]=True
+        st.session_state["uploaded_file"]=None
+        st.session_state["avvio"]=True
         st.rerun()
     with st.expander("Sezione 1"):
         st.markdown(domanda1)
