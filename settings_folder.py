37f20d303606a997931d0f83ea1ede4f8494b880
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-02-03 14:24:10 +0100

    Update settings_folder.py

--- settings_folder.py
+++ settings_folder.py
@@ -31,7 +31,41 @@ def settings_folder(cartella):
   elif cartella=="Emofilia":
     c="Emofilia"
     FOLDER_ID = "1CH9Pw0ZoWFFF2gSlOEo9UVa45akAgrz-"  
+    regional=["Stefano Stoppa, Dirigente farmacista ASST Ovest-Milano","Calogero Russo, Direttore della Farmacia Territoriale dell’ASP di Enna","Angela Dovinola, Farmacista ASL Salerno, Distretto sanitario 67 - Mercato San Severino","Nicola Petragnani, Farmacista ospedaliero presso ASL Lanciano Vasto Chieti"]
+    nome={
+      "Stefano Stoppa, Dirigente farmacista ASST Ovest-Milano":"StefanoStoppa",
+      "Calogero Russo, Direttore della Farmacia Territoriale dell’ASP di Enna":"CalogeroRusso",
+      "Angela Dovinola, Farmacista ASL Salerno, Distretto sanitario 67 - Mercato San Severino":"AngelaDovinola",
+      "Nicola Petragnani, Farmacista ospedaliero presso ASL Lanciano Vasto Chieti":"NicolaPetragnani",
+      }
+    domanda1="""
+              **Prescrizione dei farmaci**\n
+              - Come viene gestito e tracciato il flusso dell’erogazione dei farmaci off-label per le malattie rare nella vostra azienda ospedaliera o territoriale/locale per garantire l'appropriatezza delle prescrizioni? 
+              """
+    domanda2="""
+              **Erogazione e approvvigionamento**\n
+              - Come gestite l'urgenza di erogazione dei farmaci per pazienti con malattie rare?
+              - Quali sono le principali barriere burocratiche e amministrative che influenzano l'erogazione tempestiva dei farmaci?
+              """
   else:
     c="Oncoematologia"
     FOLDER_ID = "15FhRa5wa7zxNEN4GyGzJKtwc6q7jK2rR"  
+    regional=["Erika Caravaggio, farmacista UFA dell’ospedale di Alessandria","Giuseppe De Marco,  Direttore della Farmacia dell’Azienda Ospedaliera Annunziata di Cosenza","Maria Cammarota, A.O.R.N. Cardarelli/U.O.C. Farmacia-UMACA","Giancarlo Torquati, Policlinico Tor Vergata di Roma"]
+    nome={
+      "Erika Caravaggio, farmacista UFA dell’ospedale di Alessandria":"ErikaCaravaggio",
+      "Giuseppe De Marco,  Direttore della Farmacia dell’Azienda Ospedaliera Annunziata di Cosenza":"GiuseppeDeMarco",
+      "Maria Cammarota, A.O.R.N. Cardarelli/U.O.C. Farmacia-UMACA":"MariaCammarota",
+      "Giancarlo Torquati, Policlinico Tor Vergata di Roma":"GiancarloTorquati",
+      }
+    domanda1="""
+              **Creazione di mini-network (rete di Centri che trattano patologie analoghe in un’area geografica limitata)**\n
+              - Quali sarebbero i vantaggi della creazione di mini-network di area dedicati alla gestione dei farmaci per le malattie rare? Come dovrebbe essere strutturato un mini-network di area dedicato per ottimizzare l'erogazione dei farmaci?
+              - Quali sono le pratiche di formazione e sensibilizzazione del personale sanitario per evitare sprechi nella gestione dei farmaci per le malattie rare?
+                - Avete ricevuto suggerimenti pratici dal personale sanitario che potrebbero essere implementati?
+              """
+    domanda2="""
+              **Mininetwork e ottimizzazione tecnologica e pratica**\n
+              - Quali tecnologie vengono attualmente utilizzate per migliorare l'efficienza nella gestione dei farmaci per le malattie rare?
+              - E’ in grado di fornirci un esempio di efficientamento di processo che potrebbe essere implementato per migliorare la gestione dei farmaci infusivi per le malattie rare? Esistono collaborazioni con altre strutture per condividere e implementare queste misure di efficientamento? Quali?
+              """    
   return c,FOLDER_ID,regional,nome,domanda1,domanda2
