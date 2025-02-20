10c96c432fadab670230b650314404a8374c3c12
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-02-20 15:59:43 +0100

    Update app_tech.py

--- app_tech.py
+++ app_tech.py
@@ -215,10 +215,11 @@ elif mode == "Registra un nuovo audio":
     with st.expander("Sezione 1"):
         st.markdown(domanda1)
         st.components.v1.html(get_audio_recorder_html(), height=300,scrolling=True)
-        transcription_text = st_javascript("localStorage.getItem('transcription');", key="transcription_listener")
+        transcription_text = st_javascript("window.addEventListener('message', (event) => event.data.text);", key="transcription_listener")
         st.write(transcription_text)
-        if transcription_text:
-            st.text_area("Testo trascritto", st.session_state["transcription_text"], height=200)
+        if transcription_text and transcription_text != st.session_state["transcription_text"]:
+            st.session_state["transcription_text"] = transcription_text
+        st.text_area("Testo trascritto", st.session_state["transcription_text"], height=200)
     #with st.expander("Sezione 2"):
     #    st.markdown(domanda2)
     #    st.components.v1.html(get_audio_recorder_html(), height=500)
