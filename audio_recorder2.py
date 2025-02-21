0507093244743c148d4b3e826b8f1a33a117b972
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-02-21 17:30:47 +0100

    Update audio_recorder2.py

--- audio_recorder2.py
+++ audio_recorder2.py
@@ -151,9 +151,9 @@ def get_audio_recorder_html(n):
                 for (let i = event.resultIndex; i < event.results.length; i++) {
                     if (event.results[i].isFinal) {
                         finalTranscript += event.results[i][0].transcript + " ";
-                        interimTranscript += event.results[i][0].transcript + " ";
+                        interimTranscript = event.results[i][0].transcript + " ";
                     } else {
-                        interimTranscript += event.results[i][0].transcript + " ";
+                        interimTranscript = event.results[i][0].transcript + " ";
                     }
                 }
 
