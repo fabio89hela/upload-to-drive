0dacc6327522d9e463a49b5cefeaa8ad658f1262
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-02-21 17:34:29 +0100

    Update audio_recorder2.py

--- audio_recorder2.py
+++ audio_recorder2.py
@@ -151,14 +151,13 @@ def get_audio_recorder_html(n):
                 for (let i = event.resultIndex; i < event.results.length; i++) {
                     if (event.results[i].isFinal) {
                         finalTranscript += event.results[i][0].transcript + " ";
-                        interimTranscript += event.results[i][0].transcript + " ";
                     } else {
                         interimTranscript += event.results[i][0].transcript + " ";
                     }
                 }
 
                 // Aggiunge la trascrizione alla textarea globale con separatore
-                transcriptionText =  interimTranscript;
+                transcriptionText =  finalTranscript +interimTranscript;
                 document.getElementById("transcription").value = transcriptionText;
             };
 
