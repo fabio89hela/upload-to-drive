c88845f5d1fd8fb25f4b0483af5587b178754647
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-02-21 10:52:37 +0100

    Update app_tech.py

--- app_tech.py
+++ app_tech.py
@@ -223,10 +223,10 @@ elif mode == "Registra un nuovo audio":
         components.html(get_audio_recorder_html(), height=500)
 #        return_value = st_javascript("localStorage.getItem('transcription');", key="transcription_listener")
 #        st.markdown(f"Return value was: {return_value}")
-    submitted=submitted = st.form_submit_button("Submit")
-    if submitted:
-        return_value = st_javascript("localStorage.getItem('transcription');", key="transcription_listener")
-        st.markdown(f"Return value was: {return_value}")
+        submitted=submitted = st.form_submit_button("Submit")
+        if submitted:
+            return_value = st_javascript("localStorage.getItem('transcription');", key="transcription_listener")
+            st.markdown(f"Return value was: {return_value}")
     #with st.expander("Sezione 2"):
     #    st.markdown(domanda2)
     #    st.components.v1.html(get_audio_recorder_html(), height=500)
