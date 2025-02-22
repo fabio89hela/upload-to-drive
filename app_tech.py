4c53f5ff89081a96deffb3ef41d3863c548620ab
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-02-23 00:02:45 +0100

    Update app_tech.py

--- app_tech.py
+++ app_tech.py
@@ -230,12 +230,12 @@ elif mode == "Registra un nuovo audio":
             i=i+1
             transcription_text = st_javascript("""localStorage.getItem('combined_transcriptions');""",key="trascrizione_testo"+str(i))
             timestamp = st_javascript("""localStorage.getItem('update_time');""",key="tempo_trascr"+str(i))
-            if timestamp and timestamp > prev_timestamp:
-                with st.spinner("Salvando...")
-                st.session_state["transcription_text"]=transcription_text
-                st.session_state["salvato"]=True
-                break
             time.sleep(1)
+            if timestamp and timestamp > prev_timestamp:
+                with st.spinner("Salvando..."):
+                    st.session_state["transcription_text"]=transcription_text
+                    st.session_state["salvato"]=True
+                    break
     if st.session_state["salvato"]==True:
         st.markdown(st.session_state["transcription_text"])
         st.success("fatto")
