0eae5e1f6f9da9781055f7ac4b76b5d852297a19
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-02-23 08:59:15 +0100

    Update app_tech.py

--- app_tech.py
+++ app_tech.py
@@ -90,7 +90,7 @@ def riavvia(selection,restart):
 
 def get_javascript_value(js_code,testo_key):
     value = st_javascript(js_code,key=testo_key)
-    return value if value>0 else None
+    return value if len(str(value))>1 else None
 
 st.set_page_config(
     page_title="T-EMA App",
