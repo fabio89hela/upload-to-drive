7cdcf7e5ffacc39e351b87ad1245783bf0429271
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-02-24 09:57:33 +0100

    Update audio_recorder2.py

--- audio_recorder2.py
+++ audio_recorder2.py
@@ -63,7 +63,7 @@ def get_audio_recorder_html(n):
                 <button class="custom-button" id="resumeBtn-{i}" disabled>Riprendi</button>
                 <button class="custom-button" id="stopBtn-{i}" disabled>Ferma</button>
             </div>
-            <textarea class="transcription" id="transcription-{i}" placeholder="La trascrizione apparirà qui..." style="color: #34637D"></textarea>
+            <textarea class="transcription" id="transcription-{i}" placeholder="La trascrizione apparirà qui..." style="color: #34637D; border: 1px solid #34637D;border-radius: 8px;"></textarea>
             <audio id="audioPlayback-{i}" controls style="display: none;"></audio>
             <a id="downloadLink-{i}" style="display:none;">Download Audio</a>
         </div>
