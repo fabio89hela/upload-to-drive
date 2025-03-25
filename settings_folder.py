6d6cc88bd0c6a0386bd4650e990ce33c07ac3340
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-03-25 15:25:03 +0100

    Update settings_folder.py

--- settings_folder.py
+++ settings_folder.py
@@ -10,7 +10,7 @@ def settings_folder(cartella):
     c="Ematologia"
     FOLDER_ID = "1NjGZpL9XFdTdWcT-BbYit9fvOuTB6W7t"  
     domanda1="""
-              <b>Impatto della presenza di Centri Hub/Spoke e modello erogativo/assistenziale</b><br><br>
+              <b>Domande Fase 2</b><br><br><b>Impatto della presenza di Centri Hub/Spoke e modello erogativo/assistenziale</b><br><br>
               - Come viene coordinata l'erogazione di farmaci per l’ematologia tra centro Hub e centro Spoke, nella sua attuale esperienza?<br>
               - Considerando il caso in cui la diagnosi viene fatta in un Centro Hub e il farmaco ad alto costo viene erogato da un Centro Spoke o da una farmacia territoriale, è prevista una procedura ben definita tra i due Centri per poter condividere fabbisogni e consentire al Centro che dovrà prendere in carico il paziente di ricevere il budget necessario per avviare la terapia?<br>
                 - Se sì, puoi descrivere il processo?<br>
@@ -26,7 +26,7 @@ def settings_folder(cartella):
              - Ci sono iniziative in corso o pianificate per migliorare l'integrazione e la collaborazione tra i diversi centri? <br>
              - Quali sono le tecnologie o strumenti che facilitano la gestione dei pazienti e la comunicazione tra i centri? <br>
              """
-    domande_intervista=["""Nella sua regione/ASL di appartenenza è stato implementato un modello hub/spoke? Se no, riterrebbe utile implementare un modello hub/ spoke nella sua regione? <br>""",
+    domande_intervista=["""<b>Domande Fase 1</b><br><br>Nella sua regione/ASL di appartenenza è stato implementato un modello hub/spoke? Se no, riterrebbe utile implementare un modello hub/ spoke nella sua regione? <br>""",
       """Solo se ha risposto sì alla precedente domanda, quali vantaggi vede nella organizzazione Hub/Spoke?<br>""","""Quali potrebbero essere le aree di miglioramento?<br>""",
 """Quali sono le principali sfide associate alla gestione dei pazienti in un modello Hub/Spoke?<br>""","""Qual è il ruolo del farmacista ospedaliero nel modello Hub/Spoke e in particolare nel suo centro?<br>""",
 """Qual è il ruolo del farmacista territoriale nel modello Hub/Spoke e in particolare nella sua ASL di appartenenza?<br>""",
@@ -42,7 +42,7 @@ def settings_folder(cartella):
     c="Emofilia"
     FOLDER_ID = "1CH9Pw0ZoWFFF2gSlOEo9UVa45akAgrz-"  
     domanda1="""
-              <b>Prescrizione dei farmaci</b><br><br>
+              <b>Domande Fase 2</b><br><br><b>Prescrizione dei farmaci</b><br><br>
               - Come viene gestito e tracciato il flusso dell’erogazione dei farmaci off-label per le malattie rare nella vostra azienda ospedaliera o territoriale/locale per garantire l'appropriatezza delle prescrizioni? <br>
               """
     domanda2="""
@@ -58,7 +58,7 @@ def settings_folder(cartella):
               - Avete suggerimenti specifici per migliorare la procedura di prescrizione ed erogazione dei farmaci per le malattie rare? Se si, quali? <br>
               - Quali innovazioni o miglioramenti tecnologici potrebbero facilitare la gestione dei farmaci per le malattie rare? <br>
               """
-    domande_intervista=["""Qual è l'attuale procedura di erogazione e distribuzione dei farmaci per l’emofilia nella sua azienda ospedaliera o territoriale/locale? Esistono già pratiche standardizzate o modelli predefiniti da seguire per le malattie rare?<b>""", 
+    domande_intervista=["""<b>Domande Fase 1</b><br><br>Qual è l'attuale procedura di erogazione e distribuzione dei farmaci per l’emofilia nella sua azienda ospedaliera o territoriale/locale? Esistono già pratiche standardizzate o modelli predefiniti da seguire per le malattie rare?<b>""", 
     """Quali sono le principali difficoltà nella gestione dei farmaci per le malattie rare, in particolare per l'emofilia, e quali passi sarebbero prioritari per implementare un modello di armonizzazione delle procedure a livello regionale o nazionale?<b>""",
     """Come viene utilizzato il Sistema Informativo Malattie Rare (SIMaRRP) e quale tipo di supporto offre? Quali sono i limiti di questo portale? <b>""",
     """Quali criteri utilizzate attualmente per la prescrizione dei farmaci LEA ed extra-LEA per le malattie rare? Quali criteri utilizzate per i dispositivi medici? <br>"""]
@@ -66,7 +66,7 @@ def settings_folder(cartella):
     c="Oncoematologia"
     FOLDER_ID = "15FhRa5wa7zxNEN4GyGzJKtwc6q7jK2rR"  
     domanda1="""
-              <b>Creazione di mini-network (rete di Centri che trattano patologie analoghe in un’area geografica limitata)</b><br><br>
+              <b>Domande Fase 2</b><br><br><b>Creazione di mini-network (rete di Centri che trattano patologie analoghe in un’area geografica limitata)</b><br><br>
               - Quali sarebbero i vantaggi della creazione di mini-network di area dedicati alla gestione dei farmaci per le malattie rare? <br>
                 - Come dovrebbe essere strutturato un mini-network di area dedicato per ottimizzare l'erogazione dei farmaci?<br>
               - Quali sono le pratiche di formazione e sensibilizzazione del personale sanitario per evitare sprechi nella gestione dei farmaci per le malattie rare?<br>
@@ -82,7 +82,7 @@ def settings_folder(cartella):
               - E’ in grado di fornirci un esempio di efficientamento di processo che potrebbe essere implementato per migliorare la gestione dei farmaci infusivi per le malattie rare? <br>
                 - Esistono collaborazioni con altre strutture per condividere e implementare queste misure di efficientamento? Quali?<br>
               """    
-    domande_intervista=["""Quali sono le principali sfide che incontra nella gestione delle terapie infusionali per le malattie oncoematologiche rare?  In particolare:<br>
+    domande_intervista=["""<b>Domande Fase 1</b><br><br>Quali sono le principali sfide che incontra nella gestione delle terapie infusionali per le malattie oncoematologiche rare?  In particolare:<br>
 - In che modo queste sfide influiscono sull'efficienza e sugli sprechi? <br> 
 - Quali sono gli sprechi indiretti principali che possono essere evitati nella gestione delle terapie infusionali (sprechi di tempo, risorse umane o ligistiche, sprechi economici o legati a ricoveri non necessari)? <br>
 - Esistono differenze nella gestione dei farmaci inclusi nei LEA rispetto a quelli extra-LEA?  <br>""",
