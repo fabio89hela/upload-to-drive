1395a5a91c73dd3e85aaa59c49704c085dbdcaa1
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-04-07 15:56:34 +0200

    Update app_tech.py

--- app_tech.py
+++ app_tech.py
@@ -336,7 +336,6 @@ with col_center:
             if st.session_state["uploaded_file2"]:
                 with st.spinner("Caricando..."):
                     file_content = st.session_state["uploaded_file2"].read().decode("utf-8")
-                    st.text_area(file_content)
                     with tempfile.NamedTemporaryFile(delete=False, suffix=".txt", mode="w", encoding="utf-8") as temp_text_file:
                         temp_text_file.write(file_content)
                         output_path = temp_text_file.name
